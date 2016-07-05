7825d0aa8f9fd5a87434e3f60afb4d86d32f2565
Warren Buckley <warren@umbraco.com>
2016-07-05 20:24:56 +0100

    Initial commit - Testsite & Class Lib for SignalR Hub & Log4Net Appender to live

---
+++ Umbraco.DevTools.Logger/Hubs/SignalRLogAppender.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace Umbraco.DevTools.Logger.Hubs
+{
+    public class SignalRLogAppender : Hub
+    {
+        public void Send(string name, string message)
+        {
+            //Call the broadcastMessage JS Function to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        /// <summary>
+        /// For all connected SignalR clients, push the Log4Net log message to them.
+        /// In our case we will be pushing the log message to a connected Chrome DevTools client
+        /// </summary>
+        /// <param name="log">The Log4Net message</param>
+        public void SendLogMessage(string log)
+        {
+            Clients.All.appendLogMessage(log);
+        }
+
+
+        // See if we need to do anything smart when the cleitn first connects or disconnects
+        //public override Task OnConnected()
+        //{
+        //    return base.OnConnected();
+        //}
+
+        //public override Task OnDisconnected(bool stopCalled)
+        //{
+        //    return base.OnDisconnected(stopCalled);
+        //}
+    }
+}
---
+++ Umbraco.DevTools.Logger/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Umbraco.DevTools.Logger;
+using Umbraco.Web;
+
+// To use this startup class, change the appSetting value in the web.config called 
+// "owin:appStartup" to be "UmbracoDevToolsLoggerOwinStartup"
+[assembly: OwinStartup("UmbracoDevToolsLoggerOwinStartup", typeof(UmbracoDevToolsLoggerOwinStartup))]
+namespace Umbraco.DevTools.Logger
+{
+    public class UmbracoDevToolsLoggerOwinStartup : UmbracoDefaultOwinStartup
+    {
+        public override void Configuration(IAppBuilder app)
+        {
+            //Setup SignalR
+            app.MapSignalR();
+
+            //Carry on doing the normal Umbraco OWIN stuff required to make Umbraco work
+            base.Configuration(app);
+        }
+    }
+}
