df325d62db503e6947fbfae944ec8773e39ac182
Warren Buckley <warren@umbraco.com>
2016-07-06 13:45:58 +0100

    Add in Log4Net appender based of an example found online for SignalR Log4Net appender

---
+++ Umbraco.DevTools.Logger/Appender/SignalrLogAppender.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using log4net.Appender;
+using log4net.Core;
+
+namespace Umbraco.DevTools.Logger.Appender
+{
+    //Inspired by https://github.com/ChrisFulstow/log4net.SignalR/blob/master/log4net.SignalR/SignalrAppender.cs
+    //Did not use this directly as seemed that we did not need remote proxy & other bits
+
+    public class SignalrLogAppender : AppenderSkeleton
+    {
+        public SignalrLogAppender()
+        {
+            Instance = this;
+        }
+        
+        public static SignalrLogAppender Instance { get; private set; }
+        public Action<LogEntry> MessageLogged;
+
+
+        /// <summary>
+        /// When this Log4Net appender is adding a new log item
+        /// This will format the log based on conversion pattern & send the log on as an event
+        /// </summary>
+        /// <param name="loggingEvent"></param>
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            //This is a helper method that returns the raw log object in the format of the conversion pattern as a string
+            var formattedLog = RenderLoggingEvent(loggingEvent);
+
+            //Create a new object with our class below
+            //So we can send the formatted log event & the original over to SignalR or anyone else subscribed to event
+            if (MessageLogged != null)
+            {
+                MessageLogged(new LogEntry(formattedLog, loggingEvent));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Simple class to send the Log4Net log object along with formatted Log according to conversion pattern
+    /// </summary>
+    public class LogEntry
+    {
+        public LogEntry(string formttedEvent, LoggingEvent originalLogEvent)
+        {
+            FormattedEvent = formttedEvent;
+            LoggingEvent = originalLogEvent;
+        }
+
+        public string FormattedEvent { get; set; }
+        public LoggingEvent LoggingEvent { get; set; }
+    }
+}
---
+++ Umbraco.DevTools.Logger/Hubs/SignalrLogHub.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNet.SignalR;
+using Umbraco.DevTools.Logger.Appender;
+
+namespace Umbraco.DevTools.Logger.Hubs
+{
+    public class SignalrLogHub : Hub
+    {
+        public SignalrLogHub()
+        {
+            SignalrLogAppender.Instance.MessageLogged = MessageLogged;
+        }
+
+        /// <summary>
+        /// For all connected SignalR clients, push the Log4Net log message to them.
+        /// In our case we will be pushing the log message to a connected Chrome DevTools client
+        /// </summary>
+        private void MessageLogged(LogEntry logEntry)
+        {
+            //So when this is invoked by Log4Net
+            //We need to invoke the JS function appendLogMessage on all clients
+            Clients.All.appendLogMessage(logEntry);
+        }
+
+
+        /// <summary>
+        /// JUST AN EXAMPLE CODE SNIPEPT
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="message"></param>
+        public void Send(string name, string message)
+        {
+            //Call the broadcastMessage JS Function to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+        
+
+        // See if we need to do anything smart when the cleitn first connects or disconnects
+        //public override Task OnConnected()
+        //{
+        //    return base.OnConnected();
+        //}
+
+        //public override Task OnDisconnected(bool stopCalled)
+        //{
+        //    return base.OnDisconnected(stopCalled);
+        //}
+    }
+}
