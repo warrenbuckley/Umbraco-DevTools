635a4350b58c307e24e281309998760919d409cb
Warren Buckley <warren@umbraco.com>
2016-07-12 16:33:00 +0100

    Moving some bits around, to make it less specific to Logging, as I also intend to add the MiniProfiler results to the Chrome Extension

--- Umbraco.DevTools.TestSite/App_Data/Models/models.generated.cs
+++ Umbraco.DevTools.TestSite/App_Data/Models/models.generated.cs
@@ -18,7 +18,7 @@ using Umbraco.Web;
 using Umbraco.ModelsBuilder;
 using Umbraco.ModelsBuilder.Umbraco;
 
-[assembly: PureLiveAssembly, System.Reflection.AssemblyVersion("0.0.0.1")]
+[assembly: PureLiveAssembly, System.Reflection.AssemblyVersion("0.0.0.2")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
