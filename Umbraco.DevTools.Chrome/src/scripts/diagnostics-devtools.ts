85b5a34a04170ebd49163a2e7436c1f540e0cf58
Warren Buckley <warren@umbraco.com>
2016-07-27 11:24:29 +0100

    Enable Cors in Umbraco startup for WebAPIs & test JS AJAX Fetch to test we can get result from unsecured API with CORS now

--- Umbraco.DevTools.Chrome/src/scripts/diagnostics-devtools.ts
+++ Umbraco.DevTools.Chrome/src/scripts/diagnostics-devtools.ts
@@ -2,20 +2,27 @@
 
 
 // Do an AJAX HTTP Get to API
-
-$(function () {
-
-    chrome.devtools.inspectedWindow.eval("location.origin", function(result: string, exception) {
+chrome.devtools.inspectedWindow.eval("location.origin", function(result: string, exception) {
         if(!exception){
             
             //Set where /signalr is
             //So it's the current inspected tab + /Umbraco/DevTools/Public/Ping
             var apiUrl = result + "/Umbraco/DevTools/Public/Ping";
 
+             //Perform AJAX get of JSON
+            //Use JS Fetch as opposed to XMLHttpRequest
+            fetch(apiUrl, { method: 'GET', mode: 'cors'}).then(function(response) {
+                console.log('response', response);
+                return response.json();
+            }).then(function(returnedValue) {
+
+                console.log('Returned value', returnedValue);
+
+            }).catch(function(err) {
+               console.log('Problem calling API', err);
+            });
                 
         } else{
             console.log('Unable to get the tabs location.origin aka domain');
         }
     });
-
-});
---
+++ Umbraco.DevTools.Logger/UmbracoStartup.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Umbraco.Core;
+
+namespace Umbraco.DevTools.Logger
+{
+    public class UmbracoStartup : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            //Now Umbraco has booted up correcrtly & is all configured with DB etc
+            //Let's tell Umbraco we want to use CORS & that decorated API Controllers will only allow CORS requests
+            //So this will not make existing users API Controllers start accepting CORS unless they opted into this already
+
+            //We do not pass any options/params here otherwise it would make all API controllers etc use this CORS rules
+            //Want to use the opt in method of decorating with attributes
+            GlobalConfiguration.Configuration.EnableCors();
+        }
+        
+    }
+}
