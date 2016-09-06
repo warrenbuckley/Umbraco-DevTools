82173bec2bf0dadd5762f76578c6791977a21869
Warren Buckley <warren@umbraco.com>
2016-09-06 15:01:19 +0100

    WIP of fetching JSON & rendering using mustache

--- Umbraco.DevTools.Chrome/src/scripts/diagnostics-devtools.ts
+++ Umbraco.DevTools.Chrome/src/scripts/diagnostics-devtools.ts
@@ -1,28 +1,125 @@
- /// <reference path="../typings/index.d.ts" />
-
+/// <reference path="../typings/index.d.ts" />
+var cookies, generalMustacheTemplate, assembliesMustacheTemplate = "";
+chrome.devtools.inspectedWindow.eval("document.cookie", function (result, exception) {
+    if (!exception) {
+        console.log('Cookie result', result);
+        //Set the inspected page cookie to our cookies variable
+        cookies = result;
+    }
+});
 
 // Do an AJAX HTTP Get to API
-chrome.devtools.inspectedWindow.eval("location.origin", function(result: string, exception) {
-        if(!exception){
-            
-            //Set where /signalr is
-            //So it's the current inspected tab + /Umbraco/DevTools/Public/Ping
-            var apiUrl = result + "/Umbraco/DevTools/Public/Ping";
+chrome.devtools.inspectedWindow.eval("location.origin", function (result, exception) {
+    if (!exception) {
 
-             //Perform AJAX get of JSON
-            //Use JS Fetch as opposed to XMLHttpRequest
-            fetch(apiUrl, { method: 'GET', mode: 'cors'}).then(function(response) {
-                console.log('response', response);
-                return response.json();
-            }).then(function(returnedValue) {
+        preFetchMustacheTemplates();
+        getGeneralInfo(result);
+        getAssemblies(result);
+        
+    }
+    else {
+        console.log('Unable to get the tabs location.origin aka domain');
+    }
+});
 
-                console.log('Returned value', returnedValue);
-
-            }).catch(function(err) {
-               console.log('Problem calling API', err);
-            });
-                
-        } else{
-            console.log('Unable to get the tabs location.origin aka domain');
+function getGeneralInfo(baseUrl) {
+    //So it's the current inspected tab + /Umbraco/DevTools/Public/Ping
+    var apiUrl = baseUrl + "/umbraco/backoffice/DevTools/Diagnostics/GetGeneralInfo";
+    //We use the current inspected tab's cookies - & if they have auth'd to Umbraco backoffice
+    //Then we can use it for our AJAX request to the backoffice API
+    console.log('Setting cookies to ', cookies);
+    document.cookie = cookies;
+    var apiRequest = new XMLHttpRequest();
+    apiRequest.open('GET', apiUrl, true);
+    apiRequest.onreadystatechange = function () {
+        if (apiRequest.status !== 200 || apiRequest.readyState != 4) {
+            return;
         }
-    });
+        //Not logged in to Umbraco backoffice
+        if (apiRequest.status == 401 && apiRequest.readyState == 4) {
+            console.log('UNAUTH');
+        }
+        console.log('apiRequest', apiRequest);
+        console.log('Response from API', apiRequest.responseText);
+        //Parse & render the JSON response using mustache
+        appendGeneralInfo(apiRequest.responseText);
+    };
+    apiRequest.withCredentials = true;
+    apiRequest.send();
+}
+
+function getAssemblies(baseUrl) {
+    //So it's the current inspected tab + /Umbraco/DevTools/Public/Ping
+    var apiUrl = baseUrl + "/umbraco/backoffice/DevTools/Diagnostics/GetAssemblies";
+    //We use the current inspected tab's cookies - & if they have auth'd to Umbraco backoffice
+    //Then we can use it for our AJAX request to the backoffice API
+    console.log('Setting cookies to ', cookies);
+    document.cookie = cookies;
+    var apiRequest = new XMLHttpRequest();
+    apiRequest.open('GET', apiUrl, true);
+    apiRequest.onreadystatechange = function () {
+        if (apiRequest.status !== 200 || apiRequest.readyState != 4) {
+            return;
+        }
+        //Not logged in to Umbraco backoffice
+        if (apiRequest.status == 401 && apiRequest.readyState == 4) {
+            console.log('UNAUTH');
+        }
+        console.log('apiRequest', apiRequest);
+        console.log('Response from API', apiRequest.responseText);
+        //Parse & render the JSON response using mustache
+        appendAssemblies(apiRequest.responseText);
+    };
+    apiRequest.withCredentials = true;
+    apiRequest.send();
+}
+
+function preFetchMustacheTemplates() {
+    //Load & cache the mustache template
+    //Fetch Mustache Template with jQuery AJAX get
+    var generalTemplate = new XMLHttpRequest();
+    generalTemplate.open('GET', 'templates/diagnostics/general-info.mst', true);
+    generalTemplate.onreadystatechange = function () {
+        if (generalTemplate.status !== 200) {
+            console.log('Problem fetching mustache template');
+            return;
+        }
+        //The mustache template as HTML
+        generalMustacheTemplate = generalTemplate.responseText;
+        //Parse the template once on DOM load
+        //No need to fetch it every time
+        Mustache.parse(generalMustacheTemplate);
+    };
+    generalTemplate.send();
+    
+    var assembliesTemplate = new XMLHttpRequest();
+    assembliesTemplate.open('GET', 'templates/diagnostics/assemblies.mst', true);
+    assembliesTemplate.onreadystatechange = function () {
+        if (assembliesTemplate.status !== 200) {
+            console.log('Problem fetching mustache template');
+            return;
+        }
+        //The mustache template as HTML
+        assembliesMustacheTemplate = assembliesTemplate.responseText;
+        //Parse the template once on DOM load
+        //No need to fetch it every time
+        Mustache.parse(assembliesMustacheTemplate);
+    };
+    assembliesTemplate.send();
+}
+
+function appendGeneralInfo(json) {
+    //Parse the string into real JSON object for Mustache
+    var realJson = JSON.parse(json);
+    var rendered = Mustache.render(generalMustacheTemplate, realJson);
+    var data = document.getElementById('general-data');
+    data.innerHTML = rendered;
+}
+
+function appendAssemblies(json) {
+    //Parse the string into real JSON object for Mustache
+    var realJson = JSON.parse(json);
+    var rendered = Mustache.render(assembliesMustacheTemplate, realJson);
+    var data = document.getElementById('assemblies-data');
+    data.innerHTML = rendered;
+}
--- Umbraco.DevTools.Logger/Controllers/DiagnosticsApiController.cs
+++ Umbraco.DevTools.Logger/Controllers/DiagnosticsApiController.cs
@@ -1,13 +1,166 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Umbraco.Core.Configuration;
+using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi;
 
 namespace Umbraco.DevTools.Logger.Controllers
 {
-    public class DiagnosticsApiController : UmbracoAuthorizedApiController
+    [PluginController("DevTools")]
+    [EnableCors(origins: "chrome-extension://jedkgnoniejbbkcghjmfhdgappfcempk", headers: "*", methods: "*", SupportsCredentials = true)]
+    public class DiagnosticsController : UmbracoAuthorizedApiController
     {
+        [HttpGet]
+        // /umbraco/backoffice/DevTools/Diagnostics/Ping
+        public string Ping()
+        {
+            return "Secure Pong";
+        }
+
+
+        // /umbraco/backoffice/DevTools/Diagnostics/GetGeneralInfo
+        [HttpGet]
+        public GeneralInfo GetGeneralInfo()
+        {
+
+            var info = new GeneralInfo
+            {
+                UmbracoVersion = new VersionInfo
+                {
+                    Version = UmbracoVersion.Current.ToString(),
+                    VersionComment = UmbracoVersion.CurrentComment,
+                    Assembly = UmbracoVersion.AssemblyVersion
+                },
+                Server = new ServerInfo
+                {
+                    MachineName = Environment.MachineName,
+                    ProcessorCount = Environment.ProcessorCount,
+                    AspNetVersion = Environment.Version.ToString(),
+                    OperatingSystem = Environment.OSVersion.VersionString,
+                    IisVersion = HttpContext.Current.Request.ServerVariables["SERVER_SOFTWARE"]
+                },
+                Database = new DatabaseInfo
+                {
+                    Connection = DatabaseContext.ConnectionString,
+                    Type = DatabaseContext.DatabaseProvider.ToString(),
+                    Configured = DatabaseContext.IsDatabaseConfigured
+                }
+            };
+
+            return info;
+        }
+
+
+        // /umbraco/backoffice/DevTools/Diagnostics/GetAssemblies
+        [HttpGet]
+        public AssemblyInfo GetAssemblies()
+        {
+            var assemblies = new List<AssemblyItem>();
+
+            //Assemblies
+            string path = HttpContext.Current.Server.MapPath("~/bin");
+
+            //for each DLL in /bin folder add it to the list
+            foreach (string dll in Directory.GetFiles(path, "*.dll"))
+            {
+                //Load DLL (Reflection)
+                var item = Assembly.LoadFile(dll);
+
+                //Get the values from the DLL
+                var assemblyToAdd = new AssemblyItem
+                {
+                    Name = item.GetName().Name,
+                    Version = item.GetName().Version.ToString()
+                };
+
+
+                //MD5
+                using (var md5 = MD5.Create())
+                {
+                    using (var stream = File.OpenRead(item.Location))
+                    {
+                        assemblyToAdd.ChecksumMD5 = BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+                    }
+                }
+
+                //SHA1
+                using (var sha1 = SHA1.Create())
+                {
+                    using (var stream = File.OpenRead(item.Location))
+                    {
+                        assemblyToAdd.ChecksumSHA1 = BitConverter.ToString(sha1.ComputeHash(stream)).Replace("-", "").ToLower();
+                    }
+                }
+
+                //Add it to the list
+                assemblies.Add(assemblyToAdd);
+
+            }
+            
+            //Return the list
+            return new AssemblyInfo
+            {
+                Assemblies = assemblies
+            };
+        }
+        
+    }
+
+
+
+
+
+
+
+
+    
+    public class GeneralInfo
+    {
+        public VersionInfo UmbracoVersion { get; set; }
+        public ServerInfo Server { get; set; }
+        public DatabaseInfo Database { get; set; }
+    }
+
+    public class VersionInfo
+    {
+        public string Version { get; set; }
+        public string Assembly { get; set; }
+        public string VersionComment { get; set; }
+    }
+
+    public class AssemblyInfo
+    {
+        public List<AssemblyItem> Assemblies { get; set; }
+    }
+
+    public class AssemblyItem
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string ChecksumMD5 { get; set; }
+        public string ChecksumSHA1 { get; set; }
+    }
+
+    //Some basic models - TODO MOVE these
+    public class ServerInfo
+    {
+        public string MachineName { get; set; }
+        public int ProcessorCount { get; set; }
+        public string AspNetVersion { get; set; }
+        public string OperatingSystem { get; set; }
+        public string IisVersion { get; set; }
+    }
+
+    public class DatabaseInfo
+    {
+        public string Connection { get; set; }
+        public string Type { get; set; }
+        public bool Configured { get; set; }
     }
 }
